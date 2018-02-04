8d5ff1e2fc576ab423a6d271f34fbb73f699ccd6
Adam <adam@adammarcwilliams.co.uk>
2018-02-04 18:27:06 +0000

    first commit

---
+++ src/javascripts/index.js
@@ -0,0 +1,10 @@
+// import aframe from 'aframe';
+// import aframeExtras from 'aframe-extras';
+// require('aframe-teleport-controls');
+
+// import rotateImage from './rotate-image'
+
+// aframeExtras.registerAll();
+
+// aframe.registerComponent('rotate-image', rotateImage);
+
